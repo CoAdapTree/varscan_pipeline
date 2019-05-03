33c62b4403f96bc062bef815c2853b1b8c6bc322
Brandon Lind <lindb@vcu.edu>
2019-05-03 02:52:30 -0400

    GQ, missing data, MAF

--- filter_VariantsToTable.py
+++ filter_VariantsToTable.py
@@ -14,9 +14,14 @@
 # OR
 # from filter_VariantsToTable import main as remove_multiallelic
 ###
+
+### fix
+# quality filter does not ensure that qualities belong to loci that pass freq filter
+###
 """
 
-import os, sys, pandas as pd, numpy as np
+import os, sys, pandas as pd, numpy as np, math
+from tqdm import tqdm
 from coadaptree import uni
 from os import path as op
 from collections import Counter
@@ -29,7 +34,71 @@ def table(lst):
     return c
 
 
+def get_copy(df, cols):
+    return df[cols].T.copy()
+
+
+def filter_freq(df, tf, tipe):
+    """filter out loci with MAF < 0.05"""
+    # believe it or not, it's faster to do qual and freq filtering in two steps vs an 'and' statement
+    print('filtering for frequency ...')
+    df.reset_index(drop=True, inplace=True)
+    freqcols = [col for col in df.columns if '.FREQ' in col]
+    copy = get_copy(df, freqcols)
+    filtloci = []
+    for locus in tqdm(copy.columns):
+        freqs = [x for x in copy[locus].str.rstrip('%').astype('float') if not math.isnan(x)]
+        globfreq = sum(freqs)/(100*len(freqs))
+        if globfreq >= 0.05 and globfreq <= 0.95:
+            filtloci.append(locus)
+    print(f'{tf} has {len(filtloci)} {tipe}s that have MAF > 5%')
+    return df[df.index.isin(filtloci)].copy()
+
+
+def filter_missing_data(df, tf, tipe):
+    """count np.nan in .FREQ col to assess % missing data"""
+    freqcols = [col for col in df.columns if '.FREQ' in col]
+    copy = get_copy(df, freqcols)
+    keepers = []
+    thresh = math.floor(0.25 * len(freqcols))
+    for locus in tqdm(copy.columns):
+        # if there is less than 25% missing data:
+        if copy[locus].tolist().count(np.nan) < thresh:
+            keepers.append(locus)
+    df = df[df.index.isin(keepers)].copy()
+    df.index = range(len(df.index))
+    return df
+
+
+def filter_qual(df, tf, tipe):
+    """mask freqs that have GQ < 20, keep only loci with < 25% missing data"""
+    print('masking bad freqs ...')
+    qualloci = []
+    gqcols = [col for col in df.columns if '.GQ' in col]
+    thresh = math.ceil(0.75 * len(gqcols))  # assumes len(gqcols) == numpools
+    for col in tqdm(gqcols):
+        freqcol = col.replace(".GQ", ".FREQ")
+        badloci = df[col] < 20  # True if qual < 20 else False
+        df.loc[badloci, freqcol] = np.nan
+
+    print('filtering for missing data ...')
+    df = filter_missing_data(df, tf, tipe)
+
+    if len(df.index) > 0:
+        print(f'{tf} has {len(df.index)} {tipe}s that have GQ >= 20 and < 25% missing data')
+        df = filter_freq(df, tf, tipe)
+        df.index = range(len(df.index))
+    else:
+        print(f'{tf} did not have any {tipe}s that have GQ >= 20 for >= 75% of pops' +
+              '\nnot bothering to filter for freq')
+    return df
+
+
 def adjust_freqs(smalldf):
+    """
+    for loci with REF=N, set freqs of pools with REF=N in GT to np.nan,
+    set alt freqs with respect to the second alt allele
+    """
     gtcols = [col for col in smalldf.columns if 'GT' in col]
     refalt = smalldf.loc[1, 'ALT']
     # adjust freq of first alt with respect to second alt
@@ -54,7 +123,7 @@ def adjust_freqs(smalldf):
     return smalldf
 
 def main(thisfile, tablefile, tipe, ret=False):
-    print('starting %s for %s' % (op.basename(thisfile), tablefile))
+    print('starting filter_VariantsToTable.py for %s' % tablefile)
     tf = op.basename(tablefile)
 
     # load the data, create a column with CHROM-POS for locusID
@@ -97,7 +166,11 @@ def main(thisfile, tablefile, tipe, ret=False):
         print(f'{tf} has {len(ndfs.index)} biallelic {tipe}s with REF=N')
         dfs.append(df)
         df = pd.concat(dfs)
-        df.index = range(len(df.index))
+    
+    # filter for quality and missing data
+    df.index = range(len(df.index))
+    if 'varscan' in tf and tipe == 'SNP':
+        df = filter_qual(df, tf, tipe)
 
     if ret is True:
         return df
