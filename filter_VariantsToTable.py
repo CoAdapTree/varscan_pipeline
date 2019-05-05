00263c482b273f13f4ed996cf8912bd61f62e9ff
Brandon Lind <lindb@vcu.edu>
2019-05-05 03:30:18 -0400

    rm testing

--- filter_VariantsToTable.py
+++ filter_VariantsToTable.py
@@ -62,14 +62,10 @@ def filter_freq(df, tf, tipe, tablefile):
     filtloci = []
     for locus in tqdm(copy.columns):
         freqs = [x for x in copy[locus].str.rstrip('%').astype('float') if not math.isnan(x)]
-        if len(freqs) > 0:
-            globfreq = sum(freqs)/(100*len(freqs))
-        else:
-            # except loci that end up having all freqs masked
+        if len(freqs) == 0:
+            # when loci end up having all freqs masked
             continue
-        if globfreq > 1.0:
-            print('impossible globfreq, exiting')
-            exit()
+        globfreq = sum(freqs)/(100*len(freqs))
         if lowfreq <= globfreq <= highfreq:
             filtloci.append(locus)
     print(f'{tf} has {len(filtloci)} {tipe}s that have global MAF > {lowfreq*100}%')
